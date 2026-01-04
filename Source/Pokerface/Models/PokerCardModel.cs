13a1e5cfddf7faf3811c2b3f5a725e92b2b5c085
Andreas Maier <i.andreas.maier@gmail.com>
2026-01-04 15:48:38 +0100

    removed table svg completley, created background with html elements. updated relevant code snippets

--- Source/Pokerface/Models/PokerCardModel.cs
+++ Source/Pokerface/Models/PokerCardModel.cs
@@ -4,50 +4,28 @@ namespace Pokerface.Models
 {
     public class PokerCardModel
     {
-        private readonly CardProvider _cardProvider;
-
-        public double X { get; private set; }
-        public double Y { get; private set; }
-        public double Width { get; private set; }
-        public double Height { get; private set; }
-        public double Rot { get; private set; }
+        private readonly CardSvgProvider? _cardSvgProvider;             
         public string ImageUrl { get; private set; } = string.Empty;
+        public bool IsVisible { get; private set; }
 
-        public PokerCardModel(CardProvider provider)
+        public PokerCardModel()
         {
-            _cardProvider = provider;
+            
+        }
+        public PokerCardModel(CardSvgProvider provider)
+        {
+            _cardSvgProvider = provider;
+            Init(EnumCardSuit.Spade, EnumCardRank.Ace, false);
         }
 
         public void Init(EnumCardSuit suit, EnumCardRank rank, bool showFront)
         {
+            if (_cardSvgProvider == null)
+                return;
+
             ImageUrl = showFront
-                ? _cardProvider.GetFrontsideSvg(rank, suit)
-                : _cardProvider.GetBacksideSvg();
-        }
-
-        public void Update(CardProvider provider, EnumCardPositions position)
-        {
-            var rect = provider.GetCardRect(position);
-
-            X = rect.X;
-            Y = rect.Y;
-            Width = rect.Width;
-            Height = rect.Height;
-
-            Rot = 0;
-            if (position == EnumCardPositions.Player8Card1 || position == EnumCardPositions.Player8Card2)
-            {
-                Width = rect.Height;
-                Height = rect.Width;
-                Rot = 90;
-            }
-            if (position == EnumCardPositions.Player4Card1 || position == EnumCardPositions.Player4Card2)
-            {
-                Width = rect.Height;
-                Height = rect.Width;
-                Rot = 270;
-            }
+                ? _cardSvgProvider.GetFrontsideSvg(rank, suit)
+                : _cardSvgProvider.GetBacksideSvg();
         }
     }
-
 }
---
+++ Source/Pokerface/Services/CardSvgProvider.cs
@@ -0,0 +1,16 @@
+﻿
+using Pokerface.Models;
+using System.Text;
+
+namespace Pokerface.Services
+{
+    public class CardSvgProvider
+    {     
+     
+        public string GetBacksideSvg() => "images/cards/backside.svg";
+
+        public string GetFrontsideSvg(EnumCardRank rank, EnumCardSuit suit)
+            => $"images/cards/{suit}_{rank}.svg";
+    }
+
+}
